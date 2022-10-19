bb46fd98a3bc002088936ca8bacd9c2570148396
Artur César <arturcesar57@gmail.com>
2022-10-19 18:50:25 -0300

    Initial commit

---
+++ src/test/java/com/arturces/springcourseapi/SpringCourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturces.springcourseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
