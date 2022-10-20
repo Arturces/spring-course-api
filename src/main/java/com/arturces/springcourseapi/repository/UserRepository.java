5e54943da8bb8288371afec9b9da13c5961739a9
Artur César <arturcesar57@gmail.com>
2022-10-20 11:07:05 -0300

    Add User Repository

---
+++ src/main/java/com/arturces/springcourseapi/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.arturces.springcourseapi.repository;
+
+
+import com.arturces.springcourseapi.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT FROM user WHERE email = ?1 AND password = ?2")
+    public Optional<User> login(String email, String password);
+
+}
