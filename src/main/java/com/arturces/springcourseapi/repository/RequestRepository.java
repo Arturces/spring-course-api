d8d976adf9728a4cee908f16fb8076ae77781b02
Artur César <arturcesar57@gmail.com>
2022-10-20 15:20:26 -0300

    Atualizações RequestRepository

--- src/main/java/com/arturces/springcourseapi/repository/RequestRepository.java
+++ src/main/java/com/arturces/springcourseapi/repository/RequestRepository.java
@@ -1,8 +1,12 @@
 package com.arturces.springcourseapi.repository;
 
 import com.arturces.springcourseapi.domain.Request;
+import com.arturces.springcourseapi.domain.enums.RequestState;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -10,4 +14,11 @@ import java.util.List;
 public interface RequestRepository extends JpaRepository<Request, Long> {
 
     public List<Request> findAllByOwnerId(Long id);
+
+    @Transactional(readOnly = false) //disponivel para leitura e disponivel para a escrita
+    @Modifying //modificador da entidade request, modificar o estado
+    @Query("UPDATE request SET state = ?2 WHERE id = ?1")
+    public Request updateStatus(Long id, RequestState state);
+
+
 }
--- src/main/java/com/arturces/springcourseapi/repository/RequestStageRepository.java
+++ src/main/java/com/arturces/springcourseapi/repository/RequestStageRepository.java
@@ -1,10 +1,7 @@
 package com.arturces.springcourseapi.repository;
 
-import com.arturces.springcourseapi.domain.Request;
 import com.arturces.springcourseapi.domain.RequestStage;
-import com.arturces.springcourseapi.domain.enums.RequestState;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,9 +10,5 @@ import java.util.List;
 public interface RequestStageRepository extends JpaRepository<RequestStage, Long> {
 
     public List<RequestStage> findAllByRequestId(Long id);
-
-    @Query("UPDATE request SET state = ?2 WHERE id = ?1")
-    public Request updateStatus(Long id, RequestState state);
-
 
 }
