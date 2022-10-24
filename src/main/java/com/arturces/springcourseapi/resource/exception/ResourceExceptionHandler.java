c49018c2d29f48a647c670eb9d4912cd5aab42a4
Artur César <arturcesar57@gmail.com>
2022-10-24 17:42:52 -0300

    Atualização do retorno dos erros invalidos da lista

--- src/main/java/com/arturces/springcourseapi/resource/exception/ResourceExceptionHandler.java
+++ src/main/java/com/arturces/springcourseapi/resource/exception/ResourceExceptionHandler.java
@@ -10,13 +10,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @ControllerAdvice
 public class ResourceExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ApiError> handlerNotFOundException(NotFoundException ex){
+    public ResponseEntity<ApiError> handlerNotFOundException(NotFoundException ex) {
         ApiError error = new ApiError(HttpStatus.NOT_FOUND.value(), ex.getMessage(), new Date());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
@@ -24,11 +26,16 @@ public class ResourceExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        String defaultMessage = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
 
-        ApiError error = new ApiError(HttpStatus.BAD_REQUEST.value(), defaultMessage, new Date());
+        List<String> errors = new ArrayList<String>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            errors.add(error.getDefaultMessage());
+        });
+
+        String defaultMessage = "Invalid field(s)";
+
+        ApiErrorList error = new ApiErrorList(HttpStatus.BAD_REQUEST.value(), defaultMessage, new Date(), errors);
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
-
     }
 }
