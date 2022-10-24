d81d43d02ec862fb23168e07b48443555b09aeb8
Artur César <arturcesar57@gmail.com>
2022-10-24 20:57:29 -0300

    Configuração do deployable do war file

--- src/main/java/com/arturces/springcourseapi/SpringCourseApiApplication.java
+++ src/main/java/com/arturces/springcourseapi/SpringCourseApiApplication.java
@@ -2,9 +2,16 @@ package com.arturces.springcourseapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringCourseApiApplication {
+public class SpringCourseApiApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringCourseApiApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCourseApiApplication.class, args);
