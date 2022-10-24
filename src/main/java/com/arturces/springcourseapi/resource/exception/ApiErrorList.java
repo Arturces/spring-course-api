01663d6799850131c4848fedfad1cd361847c20c
Artur César <arturcesar57@gmail.com>
2022-10-24 17:41:59 -0300

    Criação da lista de erros da resposta da chamada

---
+++ src/main/java/com/arturces/springcourseapi/resource/exception/ApiErrorList.java
@@ -0,0 +1,26 @@
+package com.arturces.springcourseapi.resource.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.util.Date;
+import java.util.List;
+
+
+@Getter
+@Setter
+public class ApiErrorList  extends ApiError{
+    @Serial
+    private static final long serialVersionUID = -6812026244643145704L;
+
+    private List<String> errors;
+
+    public ApiErrorList(int code, String msg, Date date, List<String> errors){
+        super(code, msg, date);
+        this.errors = errors;
+
+    }
+
+
+}
