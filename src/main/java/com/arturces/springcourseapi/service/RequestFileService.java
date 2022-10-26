1b4eba286387239aae86c195dc7dc34bed08357f
Artur César <arturcesar57@gmail.com>
2022-10-26 11:50:45 -0300

    Integração do request file service com o s3 service

--- src/main/java/com/arturces/springcourseapi/service/RequestFileService.java
+++ src/main/java/com/arturces/springcourseapi/service/RequestFileService.java
@@ -1,20 +1,50 @@
 package com.arturces.springcourseapi.service;
 
+import com.arturces.springcourseapi.domain.Request;
 import com.arturces.springcourseapi.domain.RequestFile;
 import com.arturces.springcourseapi.model.PageModel;
 import com.arturces.springcourseapi.model.PageRequestModel;
+import com.arturces.springcourseapi.model.UploadFileModel;
 import com.arturces.springcourseapi.repository.RequestFileRepository;
+import com.arturces.springcourseapi.service.s3.S3Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class RequestFileService {
 
     @Autowired
     private RequestFileRepository fileRepository;
+
+    @Autowired
+    private S3Service s3Service;
+
+    public List<RequestFile> upload(Long requestId, MultipartFile[] files) {
+        List<UploadFileModel> uploadFiles = s3Service.upload(files);
+        List<RequestFile> requestFiles = new ArrayList<RequestFile>();
+
+        uploadFiles.forEach(uploadFile -> {
+            RequestFile file = new RequestFile();
+            file.setName(uploadFile.getName());
+            file.setLocation(uploadFile.getLocation());
+
+            Request request = new Request();
+            request.setId(requestId);
+
+            file.setRequest(request);
+            requestFiles.add(file);
+        });
+
+        return fileRepository.saveAll(requestFiles);
+
+    }
 
     public PageModel<RequestFile> listAllByRequestId(Long requestId, PageRequestModel prm) {
         Pageable pageable = PageRequest.of(prm.getPage(), prm.getSize());
