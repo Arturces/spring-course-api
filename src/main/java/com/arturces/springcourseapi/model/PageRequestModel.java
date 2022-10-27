381a268947808aee4fcec6a302ad886ffaf1b382
Artur César <arturcesar57@gmail.com>
2022-10-26 21:09:43 -0300

    Config ordenação com o sort

--- src/main/java/com/arturces/springcourseapi/model/PageRequestModel.java
+++ src/main/java/com/arturces/springcourseapi/model/PageRequestModel.java
@@ -6,7 +6,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @AllArgsConstructor
@@ -16,14 +19,32 @@ import java.util.Map;
 public class PageRequestModel {
     private int page = 0;
     private int size = 10;
+    private String sort = "";
 
-    public PageRequestModel(Map<String, String> params){
-        if(params.containsKey("page")) page = Integer.parseInt(params.get("page"));
-        if(params.containsKey("size")) size = Integer.parseInt(params.get("size"));
+    public PageRequestModel(Map<String, String> params) {
+        if (params.containsKey("page")) page = Integer.parseInt(params.get("page"));
+        if (params.containsKey("size")) size = Integer.parseInt(params.get("size"));
+        if (params.containsKey("sort")) sort = params.get("sort");
 
     }
 
-    public PageRequest toSpringPageRequest(){
-        return PageRequest.of(page,size);
+    public PageRequest toSpringPageRequest() {
+        List<Sort.Order> orders = new ArrayList<>();
+
+        String[] properties = sort.split(",");
+
+        for (String prop : properties) {
+            if (prop.trim().length() > 0) {
+                String column = prop.trim();
+
+                if (column.startsWith("-")) {
+                    column = column.replace("-", "").trim();
+
+                    orders.add(Sort.Order.desc(column));
+                } else orders.add(Sort.Order.asc(column));
+            }
+        }
+
+        return PageRequest.of(page, size, Sort.by(orders));
     }
 }
